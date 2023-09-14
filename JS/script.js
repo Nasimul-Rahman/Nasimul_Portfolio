f9efe616569b2ea83525e84139901172f519cce8
Nasimul-Rahman <nasimul@cynergon.net>
2023-09-14 22:10:14 +0600

    style overhaul

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,25 @@
-console.log("Hello")
+console.log("Hello Friend")
+
+var modalClose = document.querySelector('.modal_close');
+var projectCard = document.querySelector('.projects_card')
+
+
+if (modalClose) {
+  modalClose.addEventListener('click', function() {
+    document.querySelector('.modal').style.opacity = '0';
+    setTimeout(() => {
+        document.querySelector('.modal').style.display = "none";
+      }, 2000);
+  });
+} else {
+  console.error('Element with ID "myElementId" not found.');
+}
+
+if (projectCard) {
+    projectCard.addEventListener('click', function() {
+      document.querySelector('.modal').style.display = "flex";
+      document.querySelector('.modal').style.opacity = '1';
+    });
+  } else {
+    console.error('Element with ID "myElementId" not found.');
+  }
