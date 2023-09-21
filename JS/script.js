5344a4660f46a7f71f1a66af149050d1b553eef2
Nasimul-Rahman <nasimul@cynergon.net>
2023-09-21 13:06:58 +0600

    Modal updated

--- JS/script.js
+++ JS/script.js
@@ -1,79 +1,79 @@
 console.log("Hello Friend")
 
-var modalClose = document.querySelector('.modal_close');
-var projectCard = document.querySelector('.projects_card')
+var modals = document.querySelectorAll('.modal') 
+var modalClose = document.querySelectorAll('.modal_close')
+var projectCard = document.querySelectorAll('.projects_card')
 
+for(let i = 0; i < modals.length; i++) {
+  console.log(modals[i])
 
-if (modalClose) {
-  modalClose.addEventListener('click', function() {
-    document.querySelector('.modal').style.opacity = '0';
-    setTimeout(() => {
-        document.querySelector('.modal').style.display = "none";
-      }, 2000);
-  });
-} else {
-  console.error('Element with ID "myElementId" not found.');
-}
-
-if (projectCard) {
-    projectCard.addEventListener('click', function() {
-      document.querySelector('.modal').style.display = "flex";
-      document.querySelector('.modal').style.opacity = '1';
+  // To Close Modal
+  if (modalClose[i]) {
+    modalClose[i].addEventListener('click', function() {
+      setTimeout(function() {
+        modals[i].style.opacity = '0';
+        modals[i].style.display = "none";
+        modals[i].classList.remove('fadeIn')
+      }, 1000)
+      modals[i].classList.add('fadeOut')
     });
   } else {
     console.error('Element with ID "myElementId" not found.');
   }
 
+
+  // To Open Modal
+  if (projectCard[i]) {
+    projectCard[i].addEventListener('click', function() {
+      modals[i].style.display = "flex";
+      modals[i].style.opacity = '1';
+      modals[i].classList.remove('fadeOut')
+      modals[i].classList.add('fadeIn')
+    });
+  } else {
+    console.error('Element with ID "myElementId" not found.');
+  }
+
+}
+
+
 // When the user scrolls down then
         // slide down the navbar
-
-
-// window.onscroll = function() {scroll()};
-
-// function scroll() {
-//   console.log(document.documentElement.scrollTop)
-//   if (document.documentElement.scrollTop > 400) {
-//     document.querySelector('.navbar').className = 'navbar navbar_onscroll';
-//   } else {
-//     document.querySelector('.navbar').className = "navbar";
-//   }
-// }
 
 window.addEventListener('scroll', function() {
   console.log(document.documentElement.scrollTop)
   document.querySelector('.navbar').classList.toggle('navbar_onscroll', window.scrollY)
   document.querySelector('.active').classList.toggle('active_white', window.scrollY)
 
+  var navbarItems = document.querySelectorAll('.navbar_item')
+  
+  for(let i = 0; i < navbarItems.length; i++) {
+    navbarItems[i].classList.toggle('navbar_onscroll_navbar_item', window.scrollY)
+  }
+
   if (document.documentElement.scrollTop < 600) {
-    console.log("Home")
+    // Home
     document.getElementById('navbar_home').classList.add('active')
     document.getElementById('navbar_services').classList.remove('active_white')
     document.getElementById('navbar_projects').classList.remove('active_white')
     document.getElementById('navbar_aboutme').classList.remove('active_white')
   } else if (document.documentElement.scrollTop >= 600 && document.documentElement.scrollTop < 1150) {
-    console.log("services")
+    // Services
     document.getElementById('navbar_services').classList.add('active_white')
     document.getElementById('navbar_home').classList.remove('active_white')
     document.getElementById('navbar_projects').classList.remove('active_white')
     document.getElementById('navbar_aboutme').classList.remove('active_white')
-  } else if (document.documentElement.scrollTop >= 1150) {
-    console.log("projects")
+  } else if (document.documentElement.scrollTop >= 1150 && document.documentElement.scrollTop < 2552) {
+    // Projects
     document.getElementById('navbar_projects').classList.add('active_white')
     document.getElementById('navbar_home').classList.remove('active_white')
     document.getElementById('navbar_services').classList.remove('active_white')
     document.getElementById('navbar_aboutme').classList.remove('active_white')
-  }  
+  } else if (document.documentElement.scrollTop >= 2552) {
+    // About me
+    document.getElementById('navbar_projects').classList.remove('active_white')
+    document.getElementById('navbar_home').classList.remove('active_white')
+    document.getElementById('navbar_services').classList.remove('active_white')
+    document.getElementById('navbar_aboutme').classList.add('active_white')
+  }
 })
-
-// window.addEventListener('scroll', function() {
-//   var header = document.querySelector('.navbar');
-//   var header_logo = document.querySelector('.navbar-logo')
-//   var header_links_active = document.querySelector('.navbar-hyperlinks-active')
-//   var header_links_inactive = document.querySelectorAll('.navbar-hyperlinks-inactive')
-//   header.classList.toggle('sticky', window.scrollY)
-//   header_logo.classList.toggle('resized', window.scrollY)
-//   header_links_active.classList.toggle('underline', window.scrollY)
-//   for (let i = 0; i < 3; i++) {
-//     header_links_inactive[i].classList.toggle('inactive-underline', window.scrollY)
-//   }
-// })
